fffdfeedc50c15dc3d47a970832a466ca2fcdcb0
Tanya-Gaykevich <55203270+Tanya-Gaykevich@users.noreply.github.com>
2019-10-12 23:18:45 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,87 @@
 class TicTacToe {
-    constructor() {
 
+  constructor() {
+    this.MATRIX_SIZE = 3;
+
+    this._matrix = [ [null, null, null],
+                     [null, null, null],
+                     [null, null, null] ];
+    
+    this._currentPlayerSymbol = 'x';
+  }
+
+  changeCurrentPlayerSymbol() {
+    var CURRENT_TO_NEXT = {
+     'x': 'o',
+     'o': 'x'
+    };
+
+    this._currentPlayerSymbol = CURRENT_TO_NEXT[this.getCurrentPlayerSymbol()];
+  }
+
+  getCurrentPlayerSymbol() {
+    return this._currentPlayerSymbol;
+  }
+
+  getFieldValue(rowIndex, colIndex) {
+    return this._matrix[rowIndex][colIndex];
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.getFieldValue(rowIndex, columnIndex)) return -1;
+    this._matrix[rowIndex][columnIndex] = this._currentPlayerSymbol;
+    this.changeCurrentPlayerSymbol();
+  }
+
+  getColumn(columnIndex) {  // Get column set of matrix (my own method, not required)
+    var column = [];
+    for (var i = 0; i < this.MATRIX_SIZE; i++) {
+      column.push(this._matrix[i][columnIndex]);
+    }
+    return column.join('');
+  }
+
+  getWinner() {
+    var WINNER_X_SET = new Array(this.MATRIX_SIZE).fill('x').join('');
+    var WINNER_O_SET = new Array(this.MATRIX_SIZE).fill('o').join('');
+    var game_set = [];
+    
+    var rows = [];  // Sets of matrix after the game, which potential include winners sets
+    var columns = [];
+    var diagonal = [];
+    var diagonal_reverse = [];
+
+    for (var i = 0; i < this.MATRIX_SIZE; i++) {
+        rows.push(this._matrix[i].join(''));
+        columns.push(this.getColumn(i));
+        diagonal.push(this._matrix[i][i]);
+        diagonal_reverse.push(this._matrix[i][this.MATRIX_SIZE - i - 1]);
     }
 
-    getCurrentPlayerSymbol() {
+    game_set = game_set.concat(rows).concat(columns).concat(diagonal.join('')).concat(diagonal_reverse.join(''));
 
-    }
+    if (game_set.includes(WINNER_X_SET)) {
+        return 'x';
+    } else if (game_set.includes(WINNER_O_SET)) {
+        return 'o';
+    } else return null;
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  isDraw() {
+    return this.noMoreTurns() && !this.getWinner();
+  }
 
-    }
+  isFinished() {
+    return !!(this.getWinner()) || this.noMoreTurns();
+  }
 
-    isFinished() {
-
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  noMoreTurns() {
+    return !(this._matrix.map(
+      function(item) {
+          return item.includes(null);
+      }
+    ).includes(true));
+  }
 }
 
 module.exports = TicTacToe;
